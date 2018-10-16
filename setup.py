7de2e9353b6d330dbba562681732f466ba6e8789
Gananath R <24630963+Gananath@users.noreply.github.com>
2018-10-16 11:58:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Pyort',
+    url='https://github.com/Gananth/Pyort',
+    author='Gananath R',
+    author_email='nomail@nomail.com',
+    # Needed to actually package something
+    packages=['pyort'],
+    # Needed for dependencies
+    install_requires=['psutil','argparse','pysqlite'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    
+    entry_points = {
+        'console_scripts': [
+            'pyort=pyort.__main__:main',
+        ],
+    },
+    # The license can be anything you like
+    license='MIT',
+    description='Command line tool for monetoring all network connections',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)

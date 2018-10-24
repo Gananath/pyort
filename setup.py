34fd1d58d0f2a94f3d449d08cd52c468252b689d
Gananath R <24630963+Gananath@users.noreply.github.com>
2018-10-24 14:32:15 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['psutil','argparse','pysqlite'],
     # *strongly* suggested for sharing
-    version='0.1.6',
+    version='0.1.7',
     
     entry_points = {
         'console_scripts': [

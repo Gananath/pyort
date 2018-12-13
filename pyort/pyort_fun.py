64262181cae9eea48723e4b6cacafab30507ebf0
Gananath R <24630963+Gananath@users.noreply.github.com>
2018-12-13 10:03:42 +0530

    Add files via upload

--- pyort/pyort_fun.py
+++ pyort/pyort_fun.py
@@ -169,7 +169,7 @@ def geolite2_download(directory):
 
 
 def config_para(directory,configfile_name):
-    VERSION="0.1.7.5.9"    
+    VERSION="0.1.7.5.91"    
     # Check if there a directory exists or not
     if not os.path.exists(directory):
         os.makedirs(directory)
